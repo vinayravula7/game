3c00fcaa4c671b97a7d0bdb21e64426a1417ac8f
vinay vinny <159758948+vinayravula7@users.noreply.github.com>
2026-05-06 21:49:01 +0530

    Refactor User class to use Lombok and clean up code
    Removed unused fields and methods from User class, added Lombok annotations for boilerplate reduction.

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,11 +1,15 @@
 package com.example.demo.model;
 
 import jakarta.persistence.*;
-import java.time.LocalDateTime;
+import lombok.*;
 
 @Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder // This is required for AuthService.java to use User.builder()
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -14,73 +18,6 @@ public class User {
     private String email;
 
     private String password;
-
-    private String otp;
-
-    @Column(name = "otp_expiry", columnDefinition = "DATETIME")
-    private LocalDateTime otpExpiry;
-
-    private boolean verified = false;
-
-    public User() {
-    }
-
-    public User(Long id, String email, String password,
-                String otp, LocalDateTime otpExpiry,
-                boolean verified) {
-        this.id = id;
-        this.email = email;
-        this.password = password;
-        this.otp = otp;
-        this.otpExpiry = otpExpiry;
-        this.verified = verified;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getOtp() {
-        return otp;
-    }
-
-    public void setOtp(String otp) {
-        this.otp = otp;
-    }
-
-    public LocalDateTime getOtpExpiry() {
-        return otpExpiry;
-    }
-
-    public void setOtpExpiry(LocalDateTime otpExpiry) {
-        this.otpExpiry = otpExpiry;
-    }
-
-    public boolean isVerified() {
-        return verified;
-    }
-
-    public void setVerified(boolean verified) {
-        this.verified = verified;
-    }
+    
+    // Add any other fields you have here (username, role, etc.)
 }
